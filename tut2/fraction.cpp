954be50ff52606a5fc6acf0db0bb5948910cde52
thulanigumede <jocularmalinga@gmail.com>
2015-03-08 17:00:26 +0200

    third commit

--- tut2/fraction.cpp
+++ tut2/fraction.cpp
@@ -16,7 +16,7 @@ fraction::~fraction()
 void fraction::setden(int den)
 
 {
-	denone=den
+	denone = den
 		void fraction::setden(int den)
 
 
@@ -24,6 +24,27 @@ void fraction::setden(int den)
 
 
 }
+
+Fraction Fraction:: operator+(Fraction add)
+{
+	Fraction answer(getnum()*add.getDen() + getDen()*add.getnum(), add.getden()*getden());
+	return answer;
+}
+Fraction Fraction:: operator-(Fraction subtract)
+{
+	Fraction answer(getnum()*subtract.getden() - getden()*subtract.getnum(), subtract.getden()*getden());
+	return answer;
+}
+
+Fraction Fraction:: operator/(Fraction divide)
+{
+	Fraction answer(getden()*divide.getdenr(), getDenominator()*divide.getnum());
+	return answer;
+}
+Fraction Fraction:: operator*(Fraction multiply)
+{
+	Fraction answer(getnumr()*multiply.getnum(), multiply.getden()*getden());
+	return answer;
 
 
 
