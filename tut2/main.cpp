08701ac243a254aad1a12ed8b63402091bf21fba
thulanigumede <jocularmalinga@gmail.com>
2015-03-08 16:07:49 +0200

    first commit

---
+++ tut2/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num1, num2, den1, den2;
+	cout << "enter the first numerator";
+	cin >> num1;
+	cout << "enter the second numerator";
+	cin >> num2;
+			cout << "enter the first denominator";
+			cin >> den1;
+			cout << "enter the second denominator";
+		cin >> den2;
+
+
+
+
+
+
+
+}
